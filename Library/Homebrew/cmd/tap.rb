5c1c1baaae58e7d13111b04cdce3eb934ca6cea2
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-05-19 17:06:43 +0200

    Always try to clone my fork of a tap first
    This will also setup an upstream remote to point to the
    original repo.

--- Library/Homebrew/cmd/tap.rb
+++ Library/Homebrew/cmd/tap.rb
@@ -45,7 +45,7 @@ module Homebrew
     elsif ARGV.named.empty?
       puts Tap.names
     else
-      tap = Tap.fetch(ARGV.named[0])
+      tap = Tap.fetch(ARGV.named[0])s
       begin
         tap.install :clone_target => ARGV.named[1],
                     :full_clone   => full_clone?,
@@ -74,4 +74,51 @@ module Homebrew
       true
     end
   end
+  def install_tap_my_version(user, repo, clone_target = nil)
+    # ensure git is installed
+    Utils.ensure_git_installed!
+
+    tap = Tap.fetch user, repo
+    return false if tap.installed?
+    ohai "Tapping #{tap}"
+
+    original_remote = clone_target || "https://github.com/#{tap.user}/homebrew-#{tap.repo}"
+    my_remote = "git@github.com:stigkj/homebrew-#{tap.user}-#{tap.repo}"
+
+    args = %W[clone #{my_remote} #{tap.path}]
+    args << "--depth=1" unless ARGV.include?("--full")
+
+    begin
+      if system "git", *args
+        safe_system "git", "--git-dir=#{tap.path}/.git", "remote", "add", "upstream", original_remote
+      else
+        args[1] = original_remote
+        safe_system "git", *args
+      end
+    rescue Interrupt, ErrorDuringExecution
+      ignore_interrupts do
+        sleep 0.1 # wait for git to cleanup the top directory when interrupt happens.
+        tap.path.parent.rmdir_if_possible
+      end
+      raise
+    end
+
+    formula_count = tap.formula_files.size
+    puts "Tapped #{formula_count} formula#{plural(formula_count, "e")} (#{tap.path.abv})"
+    Descriptions.cache_formulae(tap.formula_names)
+
+    if !clone_target && tap.private?
+      puts <<-EOS.undent
+        It looks like you tapped a private repository. To avoid entering your
+        credentials each time you update, you can use git HTTP credential
+        caching or issue the following command:
+
+          cd #{tap.path}
+          git remote set-url origin git@github.com:#{tap.user}/homebrew-#{tap.repo}.git
+      EOS
+    end
+
+    true
+  end
+
 end
